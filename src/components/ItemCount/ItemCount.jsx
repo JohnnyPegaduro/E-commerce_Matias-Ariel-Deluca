a9b6071b59e4b2a1577133f52bd530cb5a539950
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-06-08 13:50:38 -0300

    Corrigiendo contador

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,46 +1,57 @@
-import React, { useState } from "react";
+import React,{useState}from'react';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import Badge from 'react-bootstrap/Badge'
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MdOutlineAddShoppingCart} from "react-icons/md";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 
 
-const ItemCount = () => {
-  //  Counter is a state initialized to 0
-    const [counter, setCounter] = useState(1)
 
-  // Function is called everytime increment button is clicked
-    const handleClick1 = () => {
-    // Counter state is incremented
-        setCounter(counter + 1)
+
+export const ItemCount = ({initial, stock, onAdd}) => {
+    const [count, setCount] = useState(initial);
+
+    const decrease = () => {
+        setCount(count - 1)
     }
 
-  // Function is called everytime decrement button is clicked
-    const handleClick2 = () => {
-    // Counter state is decremented
-    setCounter(counter - 1)
+    const increase = () => {
+        setCount(count + 1)
     }
 
-return (
+
+    return(
+        <>
+
     <Container>
         <Row className="justify-content-md-center">
             <Col md="auto"><h1>Contador</h1></Col>
         </Row>
         <Row className="justify-content-md-center">
-            <Col md="auto"><h1>{counter}</h1></Col>
+            <Col md="auto"> <Badge><h2>{count}</h2></Badge></Col>
         </Row>
         <Row className="justify-content-md-center">
             <Col xs lg="1">
-                <Button variant="success" onClick={handleClick1}>Agregar</Button>
+                <Button variant="success" disabled={count >= stock} onClick={increase} > <AiOutlinePlusCircle style={{color: 'white', fontSize: '18px'}}/>Agregar</Button>
             </Col>
             <Col xs lg="1">
-                <Button variant="danger" onClick={handleClick2}>Quitar</Button>
+                <Button variant="danger" disabled={count <= 1} onClick={decrease} ><AiOutlineMinusCircle style={{color: 'white', fontSize: '18px'}} />Quitar</Button>
+            </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+            <Col xs lg="1">
+                <Button  color="primary" outline disabled={stock <= 0} onClick={()=>onAdd(count)} > <MdOutlineAddShoppingCart style={{color: 'white', fontSize: '18px'}}/> Agregar al carrito</Button>
             </Col>
         </Row>
     </Container>
-    )
-}
+    
+        </>
+        )
+    }
 
-export default ItemCount
+    export default ItemCount;
 
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,19 @@
-
+import ItemCount from "../../components/ItemCount/ItemCount"
 
 const ItemListContainer = ({Saludo}) => {
+
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return (
-    <h1><center>{Saludo}</center></h1>
+        <>
+        
+        <h1><center>{Saludo}</center></h1>
+        
+        <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        
+        </>
     )
 }
 
